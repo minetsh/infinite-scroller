39132e658cb126160dd8ae64e2d1379faec99aa6
Felix <felix@xingren.com>
2020-06-23 23:44:53 +0800

    feat: 重新实现 Adapter

--- scripts/infinite-scroller.js
+++ scripts/infinite-scroller.js
@@ -1,23 +1,12 @@
 const list = document.getElementById('scroller');
 
-list.scrollTop = 1000;
-
-const onScroll = function (e) {
-  // console.log(e);
-  console.log(list.clientHeight, list.offsetHeight);
-};
-
-list.addEventListener('scroll', onScroll.bind(this));
-
 class Element {
   _top = 0;
 
-  constructor(element, index, top = 0) {
+  constructor(view, index, top = 0) {
     this._top = top;
     this.index = index;
-    this.element = element;
-
-    // top
+    this.view = view;
   }
 
   set top(top) {
@@ -30,7 +19,7 @@ class Element {
   }
 
   get height() {
-    return this.element.offsetHeight;
+    return this.view.offsetHeight;
   }
 }
 
@@ -40,65 +29,92 @@ class Elements {
     this.tail = null;
   }
 
-  nextTailIndex(top, height) {
-    if (!this.tail) return 0;
-    console.log(this.tail.top, top + height);
-    if (this.tail.top < top + height) {
-      return this.tail.index + 1;
+  index(down, top, height) {
+    if (down) {
+      if (!this.tail) return 0;
+      if (this.tail.top < top + height) {
+        return this.tail.index + 1;
+      }
+    } else {
+      if (!this.head) return 0;
+      if (this.head.top > top - height) {
+        return this.head.index - 1;
+      }
     }
     return -1;
   }
 
-  push(element) {
-    if (this.head === null) {
-      this.tail = this.head = element;
-      return;
+  push(down, view, index) {
+    if (this.tail === null || this.head === null) {
+      this.tail = this.head = new Element(view, index);
+      return true;
     }
-    element.top = this.tail.top + this.tail.height;
-    this.tail.next = element;
-    this.tail = element;
-  }
-}
-
-class Scroller {
-  _scrollTop = 0;
-
-  constructor(view, adapter) {
-    this.view = view;
-    view.addEventListener('scroll', this.onScroll.bind(this));
-    this.adapter = adapter;
-    this.elements = new Elements();
-    this.checkTail();
-  }
-
-  onScroll() {
-    const delta = this.view.scrollTop - this._scrollTop;
-    this._scrollTop = this.view.scrollTop;
-    console.log(delta);
-    if (delta > 0) {
-      this.checkTail();
+    const element = new Element(view, index);
+    if (down) {
+      element.top = this.tail.top + this.tail.height;
+      this.tail.next = element;
+      this.tail = element;
+    } else {
+      // 这里要斟酌下
+      element.top = this.head.top - view.offsetHeight;
+      this.head.previous = element;
+      this.head = element;
     }
-  }
-
-  checkTail() {
-    const { view, adapter, elements } = this;
-    let { scrollTop: top, clientHeight: height } = view;
-    let index = elements.nextTailIndex(top, height);
-    console.log(index);
-    while (index >= 0 && index < adapter.count()) {
-      const item = adapter.item(index);
-      const e = adapter.create(adapter.type(index));
-      adapter.bind(e, item, index);
-      elements.push(new Element(e, index));
-      this.view.appendChild(e);
-      index = elements.nextTailIndex(top, height);
-    }
+    return true;
   }
 }
 
 class Adapter {
-  constructor() {}
+  constructor(scroller) {
+    this.scroller = scroller;
+    this.scroller.addEventListener('scroll', this.onScroll.bind(this));
+    this.elements = new Elements();
+    this.scrollTop = 0;
+    this.check(true);
+  }
 
+  onScroll() {
+    const { scrollTop } = this.scroller;
+    const delta = scrollTop - this.scrollTop;
+    console.log(delta, scrollTop, this.scrollTop);
+    this.check(delta > 0);
+    this.scrollTop = scrollTop;
+  }
+
+  check(down) {
+    const { scroller, elements } = this;
+    const { scrollTop: top, clientHeight: height } = scroller;
+    let index = elements.index(down, top, height);
+    console.log('下一个位置：', index);
+    while (index >= 0 && index < this.count()) {
+      const item = this.item(index);
+      const view = this.create(this.type(index));
+      this.bind(view, item, index);
+      if (elements.push(down, view, index)) {
+        scroller.appendChild(view);
+      }
+      index = elements.index(down, top, height);
+    }
+  }
+
+  attach() {}
+
+  count() {
+    return 0;
+  }
+
+  item(index) {}
+
+  type(index) {
+    return 0;
+  }
+
+  create(type) {}
+
+  bind(view, item, index) {}
+}
+
+class ScrollAdapter extends Adapter {
   count() {
     return 100;
   }
@@ -125,4 +141,4 @@ class Adapter {
   }
 }
 
-new Scroller(list, new Adapter());
+new ScrollAdapter(list);
